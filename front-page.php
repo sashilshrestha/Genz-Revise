eccfddee51c0445919575a37ffce372ddf861e67
sashilshrestha <sashilstha5@gmail.com>
2022-02-24 19:55:59 +0545

    carousel started

--- front-page.php
+++ front-page.php
@@ -13,19 +13,13 @@ get_header();
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="https://media-exp1.licdn.com/dms/image/C4D1BAQF9i8DMUsCcBw/company-background_10000/0/1560976153674?e=2159024400&v=beta&t=mLk8Yl5nFy4WMnzSQEWpZSM5dVN5Z5e-_118IjUtrOE">
-                <div class="carousel-caption d-none d-md-block">
+                <img class="" src="https://media-exp1.licdn.com/dms/image/C4D1BAQF9i8DMUsCcBw/company-background_10000/0/1560976153674?e=2159024400&v=beta&t=mLk8Yl5nFy4WMnzSQEWpZSM5dVN5Z5e-_118IjUtrOE">
+                <div class="carousel-caption">
                     <h5>Msg</h5>
                     <p>...</p>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://media-exp1.licdn.com/dms/image/C4D1BAQF9i8DMUsCcBw/company-background_10000/0/1560976153674?e=2159024400&v=beta&t=mLk8Yl5nFy4WMnzSQEWpZSM5dVN5Z5e-_118IjUtrOE">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>...</h5>
-                    <p>...</p>
-                </div>
-            </div>
+
 
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
