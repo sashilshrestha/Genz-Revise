49c150bf735f14f6591a166b3a91446113275df6
sashilshrestha <sashilstha5@gmail.com>
2022-02-25 08:14:04 +0545

    featured section

--- front-page.php
+++ front-page.php
@@ -27,22 +27,73 @@ get_header();
                             <img class="col-md-6" src="https://media-exp1.licdn.com/dms/image/C4D1BAQF9i8DMUsCcBw/company-background_10000/0/1560976153674?e=2159024400&v=beta&t=mLk8Yl5nFy4WMnzSQEWpZSM5dVN5Z5e-_118IjUtrOE">
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <div class="row">
-                            <div class="carousel-info col-md-6">
-                                <h2>Apple iPad Pro M1(2021) launched in Nepal</h2>
-                                <p>5th generation of iPad Pro launched in Nepal With an M1 chip on It. According to Apple, this ...</p>
-                                <span class="read-more">
-                                    <a href="">Read More</a>
-                                </span>
-                            </div>
-                            <img class="col-md-6" src="https://media-exp1.licdn.com/dms/image/C4D1BAQF9i8DMUsCcBw/company-background_10000/0/1560976153674?e=2159024400&v=beta&t=mLk8Yl5nFy4WMnzSQEWpZSM5dVN5Z5e-_118IjUtrOE">
-                        </div>
-                    </div>
-
                 </div>
-
             </div>
+        </div>
+    </section>
+    <section class="featured-posts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="ft-card">
+                        <h3>Find Unique Myspace Layoutrs Nowadays</h3>
+                        <div class="bg-overlay"></div>
+                        <img src="https://images.unsplash.com/photo-1645584616771-e3180ff32aa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="ft-card">
+                        <h3>Find Unique Myspace Layoutrs Nowadays</h3>
+                        <div class="bg-overlay"></div>
+                        <img src="https://images.unsplash.com/photo-1645584616771-e3180ff32aa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="ft-card">
+                        <h3>Find Unique Myspace Layoutrs Nowadays</h3>
+                        <div class="bg-overlay"></div>
+                        <img src="https://images.unsplash.com/photo-1645584616771-e3180ff32aa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="ft-card">
+                        <h3>Find Unique Myspace Layoutrs Nowadays</h3>
+                        <div class="bg-overlay"></div>
+                        <img src="https://images.unsplash.com/photo-1645584616771-e3180ff32aa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="trending-slider">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-1">
+                    <p>Trending</p>
+                </div>
+                <div class="col-md-11">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="5000">
+                        <div class="carousel-inner">
+                            <!-- Loop Started -->
+                            <div class="carousel-item active">
+                                <div class="carousel-info">
+                                    <h2>Apple iPad Pro M1(2021) launched in Nepal</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
         </div>
     </section>
 </main>
