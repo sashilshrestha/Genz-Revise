d5c03382a8018800cabaf1be08ff1a7d5241b46f
sashilshrestha <sashilstha5@gmail.com>
2022-03-21 07:18:34 +0545

    functions added

--- functions.php
+++ functions.php
@@ -44,7 +44,9 @@ foreach ($understrap_includes as $file) {
 	require_once get_theme_file_path($understrap_inc_dir . $file);
 }
 
-// Removing Category Keyword
+/*************************************************** New Addition For Genz Revise Theme ***************************************************/
+
+// ----------------------------- Removing Category Keyword -----------------------------
 function prefix_category_title($title)
 {
 	if (is_category()) {
@@ -54,7 +56,7 @@ function prefix_category_title($title)
 }
 add_filter('get_the_archive_title', 'prefix_category_title');
 
-// Pagination Starts here
+// ----------------------------- Pagination starts here -----------------------------
 function pp_pagination_nav()
 {
 
@@ -133,13 +135,75 @@ function posts_link_attributes()
 	return 'class="switcher-pages"';
 }
 
-add_action('template_redirect', 'redirect_to_other_page');
-function redirect_to_other_page()
-{
-	if (is_page(143)) {
+// add_action('template_redirect', 'redirect_to_other_page');
+// function redirect_to_other_page()
+// {
+// 	if (is_page(143)) {
 
-		wp_redirect('"' . home_url() . '/services/messenger/"', 301);
-		///wp_redirect( 'example.com/page', 301 ); 
-		exit;
+// 		wp_redirect('"' . home_url() . '/services/messenger/"', 301);
+// 		///wp_redirect( 'example.com/page', 301 ); 
+// 		exit;
+// 	}
+// }
+
+
+// ----------------------------- Feature image in admin pannel -----------------------------
+// show featured images in dashboard
+add_image_size('genz-admin-post-featured-image', 100, 120, false);
+
+// Add the posts and pages columns filter. They both use the same function.
+add_filter('manage_posts_columns', 'genz_add_post_admin_thumbnail_column', 2);
+add_filter('manage_pages_columns', 'genz_add_post_admin_thumbnail_column', 2);
+
+// Add the column
+function genz_add_post_admin_thumbnail_column($genz_columns)
+{
+	$genz_columns['genz_thumb'] = __('Featured Image');
+	return $genz_columns;
+}
+
+// Manage Post and Page Admin Panel Columns
+add_action('manage_posts_custom_column', 'genz_show_post_thumbnail_column', 5, 2);
+add_action('manage_pages_custom_column', 'genz_show_post_thumbnail_column', 5, 2);
+
+// Get featured-thumbnail size post thumbnail and display it
+function genz_show_post_thumbnail_column($genz_columns, $genz_id)
+{
+	switch ($genz_columns) {
+		case 'genz_thumb':
+			if (function_exists('the_post_thumbnail')) {
+				echo the_post_thumbnail('genz-admin-post-featured-image');
+			} else
+				echo 'hmm… your theme doesn\'t support featured image…';
+			break;
 	}
 }
+
+// ----------------------------- Custom Excerpt  -----------------------------
+function get_excerpt_trim($num_words, $more)
+{
+	$excerpt = get_the_content();
+	$excerpt = wp_trim_words($excerpt, $num_words, $more);
+	return $excerpt;
+}
+
+// ----------------------------- Custom Wide Ads post type -----------------------------
+function gr_custom_wide_ads()
+{
+	register_post_type('Wide Ads', [
+		'rewrite' => ['slug' => 'wide_ads'],
+		'labels' => [
+			'name' => 'Wide Ads',
+			'singular_name' => 'Wide Ad',
+			'add_new_item' => 'Add Wide Ad',
+			'edit_item' => 'Edit Wide Ad',
+		],
+		'menu_icon' => 'dashicons-media-text',
+		'public' => true,
+		'has_archive' => true,
+		'show_in_rest' => true,
+		'menu_position' => 4,
+		'supports' => ['title', 'editor', 'thumbnail', 'page-attributes'],
+	]);
+}
+add_action('init', 'gr_custom_wide_ads');
